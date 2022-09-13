61780cb14994ae6f00b5642b6ada515d6f2a2a51
Colton Fox <coltonfox@coltons-mbp.wifi.uc.edu>
2022-09-13 15:09:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int age = 19;
+
+        System.out.println("Your age is " + age);
+        System.out.println("Half your age is " + age / 2.0);
+        System.out.println("Half your age is " + age % 2);
+
+        double salary = 30000.50;
+
+        System.out.println("Your salary is " + salary);
+        System.out.println("Half your wage is " + salary / 2);
+        System.out.println("Mod 2 wage is " + salary / 2);
+
+
+        // String
+
+        String name = "Colton";
+        String lname = "Fox";
+
+        System.out.println("Your name is " + name + " " + lname);
+
+
+    }
+
+}
